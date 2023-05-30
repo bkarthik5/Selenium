dcde510555dbe04c3e059639ebaf16a995a21f55
Karthik Bandaru <bandaru.karthik@gmail.com>
2023-05-30 11:19:05 -0700

    webdriver manager change

--- src/test/java/org/example/SeleniumSample.java
+++ src/test/java/org/example/SeleniumSample.java
@@ -1,15 +1,30 @@
 package org.example;
 
 import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SeleniumSample {
+    static WebDriver driver;
+    String username = System.getenv("LT_USERNAME") == null ? "LT_USERNAME": System.getenv("LT_USERNAME");
+    String accesskey = System.getenv("LT_ACCESS_KEY") == null ? "LT_ACCESS_KEY": System.getenv("LT_ACCESS_KEY");
+    String gridURL = "@hub.lambdatest.com/wd/hub";
+    @BeforeTest
+    void setup() {
+        // Set up the wWebDriverManager for chrome driver
+        WebDriverManager.chromedriver().setup();
+        // Create the driver object
+        driver = new ChromeDriver();
+    }
     @Test
-    public static void sampleTest() {
-        WebDriver driver=new ChromeDriver();
+    public void sampleTest() {
         driver.manage().window().maximize();
         driver.get("https://www.w3schools.com");
         String expectedTitle = "W3Schools Online Web Tutorials";
@@ -18,6 +33,10 @@ public class SeleniumSample {
         System.out.println("Title Validated");
         driver.findElement(By.cssSelector("#navbtn_tutorials")).click();
         driver.findElement(By.xpath("//a[@class='w3-bar-item ga-top-drop w3-button'][text()='Learn HTML']")).click();
+    }
+    @AfterTest
+    void teardown() {
+        // Close the driver
         driver.quit();
     }
 }
